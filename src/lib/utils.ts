e7c0ac78eae2b6f8c8b676c046c236f0e8e3cb9f
chauchakching <chauchakching@gmail.com>
2022-03-12 16:15:56 +0800

    style: udpate comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -42,28 +42,34 @@ export const getColorFilter = (i: number) => {
   return colorAndFilters[i][1];
 };
 
+/**
+ * For better color accessibility
+ * https://color.adobe.com/zh/create/color-accessibility
+ *
+ * Try the algo on https://codepen.io/sosuke/pen/Pjoqqp
+ */
 const colorAndFilters = [
-  // blueFilterStyle
+  // blue
   [
     '#423BE0',
     'filter: invert(18%) sepia(73%) saturate(4208%) hue-rotate(242deg) brightness(91%) contrast(94%);'
   ],
-  // redFilterStyle
+  // red
   [
     '#E31B36',
     'filter: invert(13%) sepia(79%) saturate(6156%) hue-rotate(347deg) brightness(95%) contrast(86%);'
   ],
-  // green2FilterStyle
+  // green2
   [
     '#1E9423',
     'filter: invert(43%) sepia(44%) saturate(866%) hue-rotate(73deg) brightness(94%) contrast(93%);'
   ],
-  // orangeFilterStyle
+  // orange
   [
     '#DB993D',
     'filter: invert(47%) sepia(68%) saturate(404%) hue-rotate(356deg) brightness(118%) contrast(96%);'
   ],
-  // green1FilterStyle
+  // green1
   [
     '#63E7F0',
     'filter: invert(82%) sepia(85%) saturate(2170%) hue-rotate(148deg) brightness(100%) contrast(88%);'
