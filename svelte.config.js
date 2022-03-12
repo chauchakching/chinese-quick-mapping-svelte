0b4cc58363bd885f715955ef78fb90ef1ae616f1
chauchakching <chauchakching@gmail.com>
2022-03-12 18:56:32 +0800

    feat: configure for prerender build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,10 +9,8 @@ const config = {
 
   kit: {
     adapter: adapter(),
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ['PATCH', 'DELETE']
+    prerender: {
+      default: true
     }
   }
 };
