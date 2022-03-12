891c45dfb9f3161b26689e63764baf35cc8a49c5
chauchakching <chauchakching@gmail.com>
2022-03-12 16:10:54 +0800

    fix: fix char decomposition images

--- src/lib/CharDecompositionGraph.svelte
+++ src/lib/CharDecompositionGraph.svelte
@@ -1,34 +1,40 @@
 <script lang="ts">
+  import { getColorFilter } from './utils';
+
   export let char: string;
   export let parts: string;
 
   const charColors = [
     // blue
     '#423BE0',
+    // red
+    '#E31B36',
+    // green2
+    '#1E9423',
     // orange
     '#DB993D',
     // green1
-    '#63E7F0',
-    // green2
-    '#1E9423',
-    // red
-    '#E31B36'
+    '#63E7F0'
   ];
 </script>
 
-<button class="hidden bg-white rounded flex flex-col items-center justify-center">
-  <figure class="relative mt-12" style="width: 270px; height: 192px;">
-    {#each [0, 1, 2, 3, 4, 5] as k}
-      <img
-        src={`static/chars/${char}/part_${k}.svg`}
-        class="w-full h-auto absolute opacity-10"
-        alt={`${char}-${k}`}
-      />
-    {/each}
-  </figure>
-  <div class="flex justify-center items-center">
-    {#each parts.split('') as c, i}
-      <div class="text-2xl mt-4 mb-12 mx-2" style={`color: ${charColors[i]};`}>{c}</div>
-    {/each}
-  </div>
-</button>
+<figure class="relative mt-12" style="width: 270px; height: 192px;">
+  <img
+    src={`static/chars/${char}/part_0.svg`}
+    class="w-full h-auto absolute opacity-10"
+    alt={`${char}-0`}
+  />
+  {#each [0, 1, 2, 3, 4] as k}
+    <img
+      src={`static/chars/${char}/part_${k + 1}.svg`}
+      class="w-full h-auto absolute"
+      style={getColorFilter(k)}
+      alt={`${char}-${k}`}
+    />
+  {/each}
+</figure>
+<div class="flex justify-center items-center">
+  {#each parts.split('') as c, i}
+    <div class="text-2xl mt-4 mb-12 mx-2" style={`color: ${charColors[i]};`}>{c}</div>
+  {/each}
+</div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -33,3 +33,20 @@ export const updateInputHistory = (newContent: string, inputHistory: string[]) =
       : inputHistory
   ).slice(0, 10);
 };
+
+export const getColorFilter = (i: number) => {
+  return colorFilters[i];
+};
+
+const colorFilters = [
+  // blueFilterStyle
+  'filter: invert(18%) sepia(73%) saturate(4208%) hue-rotate(242deg) brightness(91%) contrast(94%);',
+  // redFilterStyle
+  'filter: invert(13%) sepia(79%) saturate(6156%) hue-rotate(347deg) brightness(95%) contrast(86%);',
+  // green2FilterStyle
+  'filter: invert(43%) sepia(44%) saturate(866%) hue-rotate(73deg) brightness(94%) contrast(93%);',
+  // orangeFilterStyle
+  'filter: invert(47%) sepia(68%) saturate(404%) hue-rotate(356deg) brightness(118%) contrast(96%);',
+  // green1FilterStyle
+  'filter: invert(82%) sepia(85%) saturate(2170%) hue-rotate(148deg) brightness(100%) contrast(88%);'
+];
