55decd884fbefac21f4cf9d0f2bf1d1a91b85c81
chauchakching <chauchakching@gmail.com>
2023-07-29 17:37:19 +0800

    build: update e2e test config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   video: false,
 
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
