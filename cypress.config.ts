6ae5fd7c33a42ebf528aae2aa73848a3f0e0e3cd
chauchakching <chauchakching@gmail.com>
2023-07-29 19:40:53 +0800

    build: deploy action config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   video: false,
 
   e2e: {
-    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
